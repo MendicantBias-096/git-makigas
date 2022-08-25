4a6c35869f9ba7eb39fe99c828e294744c2ade43
Arturo M Jacome P <a.m.jacomep.test@gmail.com>
2022-08-25 02:50:57 -0500

    feat(c): Quinto cambio con error

--- git-makigas/c/HolaMundo.c
+++ git-makigas/c/HolaMundo.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
 
     printf("Hola Mundo Sin Error Aun Todavia");
 
-    printf("Hola Mundo Con Error");
+    printf("Hola Mundo Con Error")
 
     return 0;
 }
